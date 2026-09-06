d5a8891c6d00f25e05fa9a4f4b3bb61ac0f23aa5
vashtera <raul.aytbaev@gmail.com>
2026-09-06 12:48:36 +0500

    add premise types dict

--- backend/app/models/premises_model.py
+++ backend/app/models/premises_model.py
@@ -5,6 +5,14 @@ from database import Base
 from sqlalchemy import DECIMAL, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+PREMISE_TYPES = {
+    1: "apartments",
+    2: "house",
+    3: "country_house",
+    4: "commercial_premise",
+    5: "warehouse",
+}
+
 
 class Premise(Base):
     __tablename__ = "premises"
