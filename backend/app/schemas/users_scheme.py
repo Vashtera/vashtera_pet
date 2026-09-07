548e4af455c4da798a00bb9e6bc15b4d3250d570
vashtera <raul.aytbaev@gmail.com>
2026-09-07 13:34:30 +0500

    init users schemas

--- backend/app/schemas/users_scheme.py
+++ backend/app/schemas/users_scheme.py
@@ -2,4 +2,33 @@ from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
 
 
 class UserBase(BaseModel):
-    
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=100)
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+
+
+class UserUpdate(UserBase):
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
+
+
+class UserPrivate(UserBase):
+    id: int
+    image_file: str | None
+    image_path: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    image_file: str | None
+    image_path: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
