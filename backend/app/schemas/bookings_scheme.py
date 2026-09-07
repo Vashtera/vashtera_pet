391925d1be5545a0d8d66cab92d0ee9176c9b4c1
vashtera <raul.aytbaev@gmail.com>
2026-09-07 15:01:54 +0500

    init bookings schemas

---
+++ backend/app/schemas/bookings_scheme.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class BookingBase(BaseModel):
+    id: int
+    premise: str
+    renter: str
+    landlord: str
+    date_from: datetime
+    date_to: datetime
+    status: str
+    payment: str
+    created_at: datetime
+
+
+class BookingCreate(BaseModel):
+    premise: str
+    renter: str
+    landlord: str
+    date_from: datetime
+    date_to: datetime
+    status: str
+    payment: str
+
+
+class BookingResponse(BookingBase):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+
+class BookingUpdate(BaseModel):
+    status: str
+    date_from: datetime
+    date_to: datetime
