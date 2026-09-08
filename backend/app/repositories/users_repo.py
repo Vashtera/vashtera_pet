2ea0b03046ecf096d5d3ef039e6be89980ec0377
vashtera <raul.aytbaev@gmail.com>
2026-09-08 13:54:04 +0500

    add func get user by id

--- backend/app/repositories/users_repo.py
+++ backend/app/repositories/users_repo.py
@@ -8,3 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class UserRepo:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def get_user_by_id(self, user_id: int):
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
