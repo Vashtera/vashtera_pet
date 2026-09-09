c973d5b5271ed1c974d190d2b10df0af4fad70a3
vashtera <raul.aytbaev@gmail.com>
2026-09-09 19:32:03 +0500

    add psycopg and engine.dispose in lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 
+from app.database import engine
 from fastapi import FastAPI
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
     yield
+    await engine.dispose()
 
 
 app = FastAPI()
