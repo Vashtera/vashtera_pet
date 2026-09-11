191c7c709e238f2539da3eeee7dee0c65811aa8e
vashtera <raul.aytbaev@gmail.com>
2026-09-11 12:10:06 +0500

    init first router in premises

---
+++ backend/app/routers/premises.py
@@ -0,0 +1,17 @@
+from database import get_db
+from fastapi import APIRouter, Depends
+from schemas.premises_scheme import PremisePublic
+from services.premises_service import PremiseService
+
+router = APIRouter()
+
+
+def get_premise_service(session=Depends(get_db)):
+    return PremiseService(session)
+
+
+@router.get("premises/{premise_id}", response_model=PremisePublic)
+async def get_premise_by_id(
+    premise_id: int, db: PremiseService = Depends(get_premise_service)
+):
+    return db.get_premise_by_id(premise_id)
